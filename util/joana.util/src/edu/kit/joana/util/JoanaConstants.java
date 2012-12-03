75695ea9b38799cfb25b2b86142be0e2fa728229
Martin Mohr <martin.mohr@kit.edu>
2012-12-03 20:05:41 +0100

    JoanaConstants: initially for stubs

---
+++ util/joana.util/src/edu/kit/joana/util/JoanaConstants.java
@@ -0,0 +1,17 @@
+/**
+ * This file is part of the Joana IFC project. It is developed at the
+ * Programming Paradigms Group of the Karlsruhe Institute of Technology.
+ *
+ * For further details on licensing please read the information at
+ * http://joana.ipd.kit.edu or contact the authors.
+ */
+package edu.kit.joana.util;
+
+/**
+ * @author Martin Mohr
+ */
+public class JoanaConstants {
+	
+	public static final String STUBS_JRE_14 = "jSDG-stubs-jre1.4.jar";
+	public static final String STUBS_JRE_15 = "jSDG-stubs-jre1.5.jar";
+}

55490c73124ea117183385512a53af59cc1b20c5
Martin Mohr <martin.mohr@kit.edu>
2012-12-14 16:32:33 +0100

    explain why Demo1 fails

--- api/joana.api.testdata/src/joana/api/testdata/toy/demo/Demo1.java
+++ api/joana.api.testdata/src/joana/api/testdata/toy/demo/Demo1.java
@@ -45,6 +45,7 @@ class PublicAccessible {
 	int value;
 }
 
+
 public class Demo1 {
 
 	Secret sec1;
@@ -58,6 +59,25 @@ public class Demo1 {
 
 	public void leak1() {
 		pub.value=0;
+		/**
+		 * The references transferred to Demo1 are dependent on SECRET.
+		 * In particular, the information if the respective secrets are null
+		 * may depend on SECRET. Every time a field is read from one of the sec*
+		 * attributes of Demo1, a NullPointerException may happen. As the sec*
+		 * attributes depend on SECRET, the occurrences of NullPointerExceptions
+		 * also depend on SECRET. The occurrence of a NullPointerException in turn
+		 * influences the mere occurrence of subsequent events, so everything which
+		 * happens after the first read access to a field of a sec* attribute, also
+		 * is influenced by SECRET.
+		 * So, there may be an information leakage even if toggle() does not do anything
+		 * because the call to leak() may or may not happen depending on whether
+		 * sec1 is null, which in turn is potentially influenced by SECRET.
+		 * Subsequent calls of leak() also are, because they do not happen if sec1 is
+		 * null (the whole program simply crashes), influenced by SECRET.
+		 * Maybe, there will be a really precise Exception analysis in the future (sometime
+		 * after Dec 2012), which finds out, that independent of SECRET, no NullPointerException
+		 * will ever happen.
+		 */
 		pub.value = toggle(sec1.value1) + 28;
 		//leak(pub.value);
 	}
@@ -86,6 +106,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		Secret sec1 = new Secret(17, 0);
 		Secret sec2 = new SecretB();
+
 		Demo1 d = new Demo1(SECRET>0?sec2:sec1, SECRET>0?sec1:sec2);
 		d.leak1();
 		d.leak2(17);

7938d4aba0261c14ff0348154f252eed2691219a
Juergen Graf <juergen.graf@gmail.com>
2012-12-19 02:08:22 +0100

    remove duplicate header

--- ui/ifc/sdg/joana.ui.ifc.sdg.compiler/src/edu/kit/joana/ui/ifc/sdg/compiler/util/Activator.java
+++ ui/ifc/sdg/joana.ui.ifc.sdg.compiler/src/edu/kit/joana/ui/ifc/sdg/compiler/util/Activator.java
@@ -5,13 +5,6 @@
  * For further details on licensing please read the information at
  * http://joana.ipd.kit.edu or contact the authors.
  */
-/**
- * This file is part of the Joana IFC project. It is developed at the
- * Programming Paradigms Group of the Karlsruhe Institute of Technology.
- *
- * For further details on licensing please read the information at
- * http://joana.ipd.kit.edu or contact the authors.
- */
 package edu.kit.joana.ui.ifc.sdg.compiler.util;
 
 import java.net.MalformedURLException;
--- ui/ifc/sdg/joana.ui.ifc.sdg.latticeeditor/src/edu/kit/joana/ui/ifc/sdg/latticeeditor/parts/TransitionPart.java
+++ ui/ifc/sdg/joana.ui.ifc.sdg.latticeeditor/src/edu/kit/joana/ui/ifc/sdg/latticeeditor/parts/TransitionPart.java
@@ -5,13 +5,6 @@
  * For further details on licensing please read the information at
  * http://joana.ipd.kit.edu or contact the authors.
  */
-/**
- * This file is part of the Joana IFC project. It is developed at the
- * Programming Paradigms Group of the Karlsruhe Institute of Technology.
- *
- * For further details on licensing please read the information at
- * http://joana.ipd.kit.edu or contact the authors.
- */
 /*******************************************************************************
  * Copyright (c) 2003, 2004 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials

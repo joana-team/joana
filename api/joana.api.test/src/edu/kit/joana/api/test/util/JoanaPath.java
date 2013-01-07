2750dfc024f13f2e9a5d01d46e5c9b3d9af04a2f
Martin Mohr <martin.mohr@kit.edu>
2013-01-07 20:52:07 +0100

    joana.api.test: also make the properties file configurable by passing a suitable property

--- api/joana.api.test/src/edu/kit/joana/api/test/util/JoanaPath.java
+++ api/joana.api.test/src/edu/kit/joana/api/test/util/JoanaPath.java
@@ -22,19 +22,30 @@ public class JoanaPath {
 	public static final String ANNOTATIONS_PASSON_CLASSPATH;
 	
 	static {
-		JOANA_API_TEST_DATA_CLASSPATH = tryToLoadProperty("joana.api.testdata.classpath");
-		JOANA_MANY_SMALL_PROGRAMS_CLASSPATH = tryToLoadProperty("joana.many.small.programs.classpath");
-		ANNOTATIONS_IGNORE_CLASSPATH = tryToLoadProperty("annotations.ignore.classpath");
-		ANNOTATIONS_PASSON_CLASSPATH = tryToLoadProperty("annotations.passon.classpath");
+		String propFile = loadPropertyOrFallback("classpaths.prop-file", "classpaths.properties");
+		JOANA_API_TEST_DATA_CLASSPATH = tryToLoadProperty("joana.api.testdata.classpath", propFile);
+		JOANA_MANY_SMALL_PROGRAMS_CLASSPATH = tryToLoadProperty("joana.many.small.programs.classpath", propFile);
+		ANNOTATIONS_IGNORE_CLASSPATH = tryToLoadProperty("annotations.ignore.classpath", propFile);
+		ANNOTATIONS_PASSON_CLASSPATH = tryToLoadProperty("annotations.passon.classpath", propFile);
 	}
 	
-	private static String tryToLoadProperty(String key) {
+	private static String loadPropertyOrFallback(String key, String fallback) {
+		String ret = System.getProperty(key);
+		if (ret != null) {
+			return ret;
+		} else {
+			return fallback;
+		}
+	}
+	
+	private static String tryToLoadProperty(String key, String propertiesFile) {
+		System.out.println("Loading property " + key + " from file " + propertiesFile + "... ");
 		String jPath = System.getProperty(key);
 		if (jPath != null) {
 			return jPath;
 		} else {
 			try {
-				InputStream propertyStream = new FileInputStream("classpaths.properties");
+				InputStream propertyStream = new FileInputStream(propertiesFile);
 				Properties p = new Properties();
 				p.load(propertyStream);
 				jPath = p.getProperty(key);
@@ -43,7 +54,7 @@ public class JoanaPath {
 			if (jPath != null) {
 				return jPath;
 			} else {
-				throw new IllegalStateException("Property '"+ key + "' not provided! Either add a 'project.properties' with an appropriate specification or provide property via -D flag to the jvm!");
+				throw new IllegalStateException("Property '"+ key + "' not provided! Either add file '" + propertiesFile + "' with an appropriate specification or provide property via -D flag to the jvm!");
 			}
 		}
 	}

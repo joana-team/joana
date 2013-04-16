846cd57aa42366b4f3da7828353d08576ab4f0b5
Martin Mohr <martin.mohr@kit.edu>
2013-04-16 13:39:54 +0200

    really add moved MHPType enum

---
+++ ifc/sdg/joana.ifc.sdg.mhpoptimization/src/edu/kit/joana/ifc/sdg/mhpoptimization/MHPType.java
@@ -0,0 +1,12 @@
+/**
+ * This file is part of the Joana IFC project. It is developed at the
+ * Programming Paradigms Group of the Karlsruhe Institute of Technology.
+ *
+ * For further details on licensing please read the information at
+ * http://joana.ipd.kit.edu or contact the authors.
+ */
+package edu.kit.joana.ifc.sdg.mhpoptimization;
+
+public enum MHPType {
+	NONE, SIMPLE, PRECISE;
+}

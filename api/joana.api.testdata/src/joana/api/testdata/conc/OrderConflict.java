63572e854ed1f50a3f92db5bf8249cde96d7d4eb
Martin Mohr <martin.mohr@kit.edu>
2013-07-22 13:16:13 +0200

    some more test programs - partly unfinished

---
+++ api/joana.api.testdata/src/joana/api/testdata/conc/OrderConflict.java
@@ -0,0 +1,43 @@
+/**
+ * This file is part of the Joana IFC project. It is developed at the
+ * Programming Paradigms Group of the Karlsruhe Institute of Technology.
+ *
+ * For further details on licensing please read the information at
+ * http://joana.ipd.kit.edu or contact the authors.
+ */
+package joana.api.testdata.conc;
+
+/**
+ * @author Martin Mohr
+ */
+public class OrderConflict {
+	
+	static class Thread1 extends Thread {
+		public void run() {
+			x = 1;
+			while (x > 0) {
+				x--;
+			}
+			System.out.println("Thread 1 ready.");
+		}
+	}
+	
+	static class Thread2 extends Thread {
+		public void run() {
+			System.out.println("Thread 2 ready.");
+		}
+	}
+	
+	static int x;
+	
+	public static void main(String[] args) throws InterruptedException {
+		Thread1 t1 = new Thread1();
+		Thread2 t2 = new Thread2();
+		
+		t1.start(); t1.join();
+		t2.start();
+	}
+	
+	
+	
+}

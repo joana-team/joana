372ef20bd86e6ca2e78216cf21965369a3f4a971
Martin Mohr <martin.mohr@kit.edu>
2013-11-22 15:47:56 +0100

    fix add logic for SDGCall; also cover return and exception node

--- api/joana.api/src/edu/kit/joana/api/sdg/SDGCall.java
+++ api/joana.api/src/edu/kit/joana/api/sdg/SDGCall.java
@@ -7,6 +7,7 @@
  */
 package edu.kit.joana.api.sdg;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.SortedMap;
@@ -24,8 +25,10 @@ import edu.kit.joana.ifc.sdg.util.JavaType;
 public class SDGCall extends SDGInstruction {
 
 	private SortedMap<Integer, SDGActualParameter> actParams = new TreeMap<Integer, SDGActualParameter>();
+	private SDGNode returnNode;
+	private SDGNode exceptionNode;
 	private Set<JavaMethodSignature> possibleTargets = new HashSet<JavaMethodSignature>();
-	
+
 	public SDGCall(SDGMethod owner, SDGNode callNode, int index) {
 		super(owner, callNode, index);
 	}
@@ -35,35 +38,72 @@ public class SDGCall extends SDGInstruction {
 			throw new IllegalArgumentException("Illegal kind: " + act.getKind());
 		} else {
 			int index = BytecodeLocation.getRootParamIndex(act.getBytecodeName());
-			JavaType type = JavaType.parseSingleTypeFromString(act.getType());
-			SDGActualParameter actP;
-			if (!actParams.containsKey(index)) {
-				actP = new SDGActualParameter(this, index, type);
-			} else {
-				actP = actParams.get(index);
+			if (index >= 0) {
+				JavaType type = JavaType.parseSingleTypeFromString(act.getType());
+				SDGActualParameter actP;
+				if (!actParams.containsKey(index)) {
+					actP = new SDGActualParameter(this, index, type);
+					actParams.put(index, actP);
+				} else {
+					actP = actParams.get(index);
+				}
 				if (act.getKind() == Kind.ACTUAL_IN) {
 					actP.setInRoot(act);
 				} else {
 					actP.setOutRoot(act);
 				}
+			} else {
+				if (act.getBytecodeName().equals(BytecodeLocation.RETURN_PARAM)) {
+					this.returnNode = act;
+				} else if (act.getBytecodeName().equals(BytecodeLocation.EXCEPTION_PARAM)) {
+					this.exceptionNode = act;
+				}
 			}
 		}
 	}
-	
+
 	void addPossibleCallTarget(JavaMethodSignature pTgt) {
 		possibleTargets.add(pTgt);
 	}
 	
+	public Collection<SDGActualParameter> getActualParameters() {
+		return actParams.values();
+	}
+	
+	public SDGNode getReturn() {
+		return returnNode;
+	}
+	
+	public SDGNode getExceptionNode() {
+		return exceptionNode;
+	}
+
 	/**
-	 * Returns whether this instruction <p/>
+	 * Returns whether this instruction
+	 * <p/>
 	 * <ol>
-	 * <li>is a call instruction </li>
+	 * <li>is a call instruction</li>
 	 * <li>has the method with the given signature as possible call target</li>
 	 * </ol>
-	 * @param target 
-	 * @return {@code true}, if this instruction is a call instruction having the method with the given signature as possible target, {@code false} otherwise
+	 * 
+	 * @param target
+	 * @return {@code true}, if this instruction is a call instruction having
+	 *         the method with the given signature as possible target,
+	 *         {@code false} otherwise
 	 */
 	public boolean possiblyCalls(JavaMethodSignature target) {
 		return possibleTargets.contains(target);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * edu.kit.joana.api.sdg.SDGInstruction#acceptVisitor(edu.kit.joana.api.
+	 * sdg.SDGProgramPartVisitor, java.lang.Object)
+	 */
+	@Override
+	public <R, D> R acceptVisitor(SDGProgramPartVisitor<R, D> v, D data) {
+		return v.visitCall(this, data);
 	}
 }

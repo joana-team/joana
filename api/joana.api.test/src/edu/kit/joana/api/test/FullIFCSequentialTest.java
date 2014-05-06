e352fbd9ca81fdf4897701f6fb3af6a748a0c8fe
Juergen Graf <juergen.graf@gmail.com>
2014-05-06 19:48:03 +0200

    with static field killing definitions now working, we get fewer security leaks - adjust testcases

--- api/joana.api.test/src/edu/kit/joana/api/test/FullIFCSequentialTest.java
+++ api/joana.api.test/src/edu/kit/joana/api/test/FullIFCSequentialTest.java
@@ -103,7 +103,7 @@ public class FullIFCSequentialTest {
 					"sequential.PraktomatLeak$Review.failures");
 			Collection<? extends IViolation<SecurityNode>> illegal = ana.doIFC();
 			assertFalse(illegal.isEmpty());
-			assertEquals(8, illegal.size());
+			assertEquals(6, illegal.size());
 		} catch (ApiTestException e) {
 			e.printStackTrace();
 			fail(e.getMessage());

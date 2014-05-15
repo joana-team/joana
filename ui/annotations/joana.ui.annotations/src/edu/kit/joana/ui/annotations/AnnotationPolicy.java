d1a281d2949b2248dc2ba3d3106f4d9b9aa7232c
Juergen Graf <juergen.graf@gmail.com>
2014-05-15 14:51:17 +0200

    use joana project templates in new annotations project

--- ui/annotations/joana.ui.annotations/src/edu/kit/joana/ui/annotations/AnnotationPolicy.java
+++ ui/annotations/joana.ui.annotations/src/edu/kit/joana/ui/annotations/AnnotationPolicy.java
@@ -1,3 +1,14 @@
+/**
+ * This file is part of the Joana IFC project. It is developed at the
+ * Programming Paradigms Group of the Karlsruhe Institute of Technology.
+ *
+ * For further details on licensing please read the information at
+ * http://joana.ipd.kit.edu or contact the authors.
+ */
 package edu.kit.joana.ui.annotations;
 
+/**
+ * 
+ * @author Martin Hecker <martin.hecker@kit.edu>
+ */
 public enum AnnotationPolicy { ANNOTATE_USAGES, ANNOTATE_CALLE }

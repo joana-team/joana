1dc885ccacdb204e8fb82cf3b6561b9d9300b079
Martin Mohr <martin.mohr@kit.edu>
2014-06-23 18:38:17 +0200

    more utility methods for parameter nodes

--- api/joana.api/src/edu/kit/joana/api/sdg/SDGParameterUtils.java
+++ api/joana.api/src/edu/kit/joana/api/sdg/SDGParameterUtils.java
@@ -9,11 +9,14 @@ package edu.kit.joana.api.sdg;
 
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 import edu.kit.joana.ifc.sdg.graph.SDG;
 import edu.kit.joana.ifc.sdg.graph.SDGEdge;
 import edu.kit.joana.ifc.sdg.graph.SDGNode;
+import edu.kit.joana.ifc.sdg.graph.SDGNode.Kind;
+import edu.kit.joana.ifc.sdg.util.BytecodeLocation;
 
 /**
  * Provides utility code used for parameters.
@@ -51,6 +54,57 @@ class SDGParameterUtils {
 			}
 		}
 		return false;
+	}
+
+	/**
+	 * Computes all the root parameter nodes or static field nodes from which the given node may be reached using only
+	 * {@link SDGEdge.PARAMETER_STRUCTURE} edges
+	 * @param node node to check - should be of kind ACT_IN or ACT_OUT to get a reasonable result
+	 * @param sdg sdg in which to search
+	 * @return all root parameter nodes or static field nodes from which the given node may be reached using only {@link SDGEdge.PARAMETER_STRUCTURE} edges
+	 */
+	static List<SDGNode> collectPsBWReachableRootParameters(SDGNode node, SDG sdg) {
+		LinkedList<SDGNode> ret = new LinkedList<SDGNode>();
+		LinkedList<SDGNode> worklist = new LinkedList<SDGNode>();
+		Set<SDGNode> visited = new HashSet<SDGNode>();
+		worklist.add(node);
+		while (!worklist.isEmpty()) {
+			SDGNode next = worklist.poll();
+			visited.add(next);
+			if (next.getBytecodeIndex() == BytecodeLocation.ROOT_PARAMETER) {
+				ret.add(next);
+			} else {
+				for (SDGEdge out : sdg.incomingEdgesOf(next)) {
+					if (out.getKind() == SDGEdge.Kind.PARAMETER_STRUCTURE && !visited.contains(out.getSource())) {
+						worklist.add(out.getSource());
+					}
+				}
+			}
+		}
+		return ret;
+	}
+
+	/**
+	 * Locates the method call node to which the given node belongs. The given node must be of kind
+	 * {@link SDGNode.Kind#ACTUAL_IN} or {@link SDGNode.Kind#ACTUAL_OUT} and a root parameter or a
+	 * static field.
+	 * @param node node to find corresponding call of
+	 * @param sdg sdg in which to search
+	 * @return the method call node to which the given node belongs
+	 */
+	static SDGNode locateCall(SDGNode node, SDG sdg) {
+		if (node.getKind() != SDGNode.Kind.ACTUAL_IN && node.getKind() != SDGNode.Kind.ACTUAL_OUT) {
+			throw new IllegalArgumentException(String.format("Given node must be of kind %s or %s!", SDGNode.Kind.ACTUAL_IN.toString(), SDGNode.Kind.ACTUAL_OUT.toString()));
+		}
+		if (node.getBytecodeIndex() != BytecodeLocation.ROOT_PARAMETER && node.getBytecodeIndex() != BytecodeLocation.STATIC_FIELD) {
+			throw new IllegalArgumentException("Given node must denote a root parameter or a static field!");
+		}
+		for (SDGEdge eIn : sdg.incomingEdgesOf(node)) {
+			if (eIn.getKind() == SDGEdge.Kind.CONTROL_DEP_EXPR && eIn.getSource().getKind() == Kind.CALL) {
+				return eIn.getSource();
+			}
+		}
+		throw new IllegalStateException("This should not happen!");
 	}
 
 }

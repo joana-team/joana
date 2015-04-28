984b77b0e8889db01e1449000667134379ecefec
Juergen Graf <juergen.graf@gmail.com>
2015-04-28 13:32:14 +0200

    add extended statistics to line number slicer and add filter for relevant lines

--- wala/joana.wala.eval/src/edu/kit/joana/wala/eval/util/LineNrSlicer.java
+++ wala/joana.wala.eval/src/edu/kit/joana/wala/eval/util/LineNrSlicer.java
@@ -10,6 +10,7 @@ package edu.kit.joana.wala.eval.util;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -17,6 +18,7 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 import org.jgrapht.traverse.BreadthFirstIterator;
@@ -84,9 +86,15 @@ public class LineNrSlicer {
 	private SDG sdg;
 	private Slicer slicerBack;
 	private Slicer slicerForw;
+	private final boolean extendedStats;
 
 	public LineNrSlicer(final SDG sdg) {
+		this(sdg, false);
+	}
+	
+	public LineNrSlicer(final SDG sdg, final boolean extendedStats) {
 		this.sdg = sdg;
+		this.extendedStats = extendedStats;
 		this.slicerBack = new SummarySlicerBackward(sdg);
 		this.slicerForw = new SummarySlicerBackward(sdg);
 	}
@@ -143,7 +151,7 @@ public class LineNrSlicer {
 		TreeSet<Line> lines = new TreeSet<Line>();
 
 		for (SDGNode node : nodes) {
-			if (node.getSource() != null) {
+			if (isRelevantLine(node)) {
 				Line line = new Line(node.getSource(), node.getSr());
 				lines.add(line);
 			}
@@ -203,16 +211,83 @@ public class LineNrSlicer {
 		public int numberOfLines;
 		public long avgLinesPerSlice;
 		public double percentInSlice;
+		public SortedSet<LineInSlice> perLineResults; // only present if LineNrSlicer started with extendedStats enabled
+		
+		public String toString() {
+			return avgLinesPerSlice + " of " + numberOfLines + " in average slice. (" + percentInSlice + "%)";
+		}
+		
+		public void printPerLineResult(final PrintStream out) {
+			if (perLineResults == null) {
+				out.println("no per line results available.");
+			} else {
+				out.println("slicing stats per line:");
+				for (final LineInSlice lis : perLineResults) {
+					out.println(lis);
+				}
+			}
+		}
+	}
+	
+	public static class LineInSlice implements Comparable<LineInSlice> {
+		public int count;
+		public final Line line;
+		
+		public LineInSlice(final Line line) {
+			this.line = line;
+		}
+		
+		public int hashCode() {
+			return line.hashCode() * 4177;
+		}
+		
+		public boolean equals(Object o) {
+			if (this == o) {
+				return true;
+			}
+			
+			if (o instanceof LineInSlice) {
+				LineInSlice lis = (LineInSlice) o;
+				return line.equals(lis.line);
+			}
+			
+			return false;
+		}
+		
+		public int compareTo(LineInSlice other) {
+			int diff = count - other.count;
+			
+			if (diff != 0) {
+				return diff;
+			}
+			
+			return line.compareTo(other.line);
+		}
+		
+		public String toString() {
+			return count + "\t" + line;
+		}
+			
+	}
+	
+	private static boolean isExcluded(final String str) {
+		return str.contains("java/lang") || str.contains("java/io") || str.contains("java/util")
+				|| str.contains("com/ibm/wala") || str.contains("sun/reflect") || str.contains("java/security");
+	}
+	
+	private static boolean isRelevantLine(final SDGNode node) {
+		return node.getSource() != null && node.getSr() >= 0 && !isExcluded(node.getSource());
 	}
 	
 	public Result heavySliceBackw() {
 		final Result r = new Result();
 		final Set<Line> lines = new TreeSet<Line>();
 		final Map<Line,Set<SDGNode>> line2nodes = new HashMap<>();
+		final Map<Line,LineInSlice> line2lis = (extendedStats ? new HashMap<Line, LineInSlice>() : null);
 		final BreadthFirstIterator<SDGNode, SDGEdge> it = new BreadthFirstIterator<SDGNode, SDGEdge>(sdg);
 		while (it.hasNext()) {
 			final SDGNode node = it.next();
-			if (node.getSource() != null && node.getSr() >= 0) {
+			if (isRelevantLine(node)) {
 				Line line = new Line(node.getSource(), node.getSr());
 				lines.add(line);
 				Set<SDGNode> nodes = line2nodes.get(line);
@@ -238,22 +313,29 @@ public class LineNrSlicer {
 				nodes.addAll(toAdd);
 			}
 		}
+
+		if (extendedStats) {
+			for (final Line l : lines) {
+				LineInSlice lis = new LineInSlice(l);
+				line2lis.put(l, lis);
+			}
+		}
 		
 		r.numberOfLines = lines.size();
 		//System.out.println(lines.size() + " different sourcecode lines found.");
 		long sliceTotal = 0;
-//		long linesCurrent = 0;
-//		double percentCurrent;
 		final LinkedList<Line> worklist = new LinkedList<>();
 		worklist.addAll(lines);
 		Collections.shuffle(worklist);
 		for (final Line l : worklist) {
 			final Set<Line> slice = sliceBackward(l, line2nodes);
-//			linesCurrent++;
+			if (extendedStats) {
+				for (Line sliceline : slice) {
+					LineInSlice lis = line2lis.get(sliceline);
+					lis.count++;
+				}
+			}
 			sliceTotal += slice.size();
-//			double avgLines = (double) sliceTotal / (double) linesCurrent;
-//			percentCurrent = (100.0 * avgLines) / (double) r.numberOfLines;
-//			if (linesCurrent % 20 == 0) { System.out.print("[" + NF.format(percentCurrent) + "%]"); }
 			//System.out.println(l + ": " + size);
 		}
 
@@ -261,6 +343,11 @@ public class LineNrSlicer {
 //		System.out.println("Total num of slices: " + lines.size());
 		r.avgLinesPerSlice = sliceTotal / r.numberOfLines;
 		r.percentInSlice = (100.0 * r.avgLinesPerSlice) / ((double) r.numberOfLines);
+		if (extendedStats) {
+			final SortedSet<LineInSlice> sortedLines = new TreeSet<>();
+			sortedLines.addAll(line2lis.values());
+			r.perLineResults = sortedLines;
+		}
 //		System.out.println("Avg. lines per slice: " + linesPerSlice + "(" + percent + "%)");
 		return r;
 	}

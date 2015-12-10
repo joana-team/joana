29b9ecc77aa123ed7ee1ef4937ec126ef6eba80f
Martin Hecker <martin.hecker@kit.edu>
2015-12-10 18:16:46 +0100

    first version of an analysis that computes securit levelts for the relative timing of two nodes

---
+++ src/edu/kit/joana/ifc/orlsod/TimimgClassificationChecker.java
@@ -0,0 +1,287 @@
+package edu.kit.joana.ifc.orlsod;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.jgrapht.alg.TransitiveClosure;
+
+import edu.kit.joana.ifc.sdg.graph.SDG;
+import edu.kit.joana.ifc.sdg.graph.SDGEdge;
+import edu.kit.joana.ifc.sdg.graph.SDGNode;
+import edu.kit.joana.ifc.sdg.graph.slicer.conc.CFGForward;
+import edu.kit.joana.ifc.sdg.graph.slicer.conc.I2PBackward;
+import edu.kit.joana.ifc.sdg.graph.slicer.graph.CFG;
+import edu.kit.joana.ifc.sdg.graph.slicer.graph.building.ICFGBuilder;
+import edu.kit.joana.ifc.sdg.graph.slicer.graph.threads.PreciseMHPAnalysis;
+import edu.kit.joana.ifc.sdg.lattice.IStaticLattice;
+import edu.kit.joana.ifc.sdg.lattice.LatticeUtil;
+import edu.kit.joana.util.Pair;
+
+public class TimimgClassificationChecker<L> {
+
+	/** the lattice which provides the security levels we annotate nodes with */
+	protected final IStaticLattice<L> secLattice;
+
+	/** the SDG we want to check */
+	protected final SDG sdg;
+
+	/** user-provided source annotations */
+	protected final Map<SDGNode, L> srcAnn;
+
+	/** user-provided sink annotations */
+	protected final Map<SDGNode, L> snkAnn;
+
+	/** maps each node to its so-called <i>probabilistic influencers</i> */
+	protected final ProbInfComputer probInf;
+
+	protected final CFG icfg;
+	protected final Map<SDGNode, Collection<SDGNode>> transClosure;
+	
+	protected final Map<SDGNode, Collection<SDGNode>> timingDependence;
+	protected final Map<Pair<SDGNode,SDGNode>, Collection<? extends SDGNode>> chops;
+	
+	protected final SimpleTCFGChopper tcfgChopper;
+	protected final CFGForward tcfgForwardSlicer;
+
+	
+	private final ICDomOracle cdomOracle;
+	
+	protected final PreciseMHPAnalysis mhp;
+	
+	/** classification which is computed in a fixed-point iteration */
+	protected Map<SDGNode, L> cl;
+	
+	protected Map<Pair<SDGNode, SDGNode>, L> clt; 
+
+	public TimimgClassificationChecker(SDG sdg, IStaticLattice<L> secLattice, Map<SDGNode, L> srcAnn, Map<SDGNode, L> snkAnn, ProbInfComputer probInf, PreciseMHPAnalysis mhp, ICDomOracle cdomOracle) {
+		this.sdg = sdg;
+		this.secLattice = secLattice;
+		this.srcAnn = srcAnn;
+		this.snkAnn = snkAnn;
+		this.probInf = probInf;
+		this.mhp = mhp;
+		this.cdomOracle = cdomOracle;
+		
+		this.transClosure = new HashMap<>();
+		
+		// TODO: try not to annoy Jürgen by "doing Work" in a constructor!
+		this.icfg = ICFGBuilder.extractICFG(sdg);
+		
+		this.tcfgChopper = new SimpleTCFGChopper(icfg);
+		this.tcfgForwardSlicer = new CFGForward(icfg);
+		this.chops = new HashMap<>();
+		this.timingDependence = new HashMap<>();
+		
+		for (SDGNode n : icfg.vertexSet()) {
+			final List<SDGEdge> edges =
+				icfg.outgoingEdgesOf(n)
+				    .stream()
+				    .filter(e -> !e.getKind().equals(SDGEdge.Kind.FORK))
+				    .collect(Collectors.toList());
+			
+			final int nr = edges.size();
+			
+			if (!(nr==0 || nr == 1 || nr == 2 || n.kind.equals(SDGNode.Kind.ENTRY))) {
+				int x= nr*2;
+			}
+			
+			// ENTRY Nodes may have three successors:
+			//       i)   A formal-in node, which eventually leads to the procedured "real" control flow
+			//       ii)  The EXIT node, as required for control-deps 
+			//       iii) A formal-out node, also leading to to EXIT Node
+			// TODO: possibly fix this in the graph construction
+			//assert (nr==0 || nr == 1 || nr == 2 || n.kind.equals(SDGNode.Kind.ENTRY));
+			
+			if (nr == 2) {
+				final SDGNode n2 = edges.get(0).getTarget();
+				final SDGNode n3 = edges.get(1).getTarget();
+				
+				transClosure.computeIfAbsent(n2, tcfgForwardSlicer::slice);
+				transClosure.computeIfAbsent(n3, tcfgForwardSlicer::slice);
+				
+				final List<SDGNode> dependentNodes =
+					transClosure.get(n2)
+					            .stream()
+					            .filter(transClosure.get(n3)::contains)
+					            .collect(Collectors.toList());
+				timingDependence.put(n, dependentNodes);
+			} else {
+				timingDependence.put(n, Collections.emptySet());
+			}
+		}
+		
+	}
+
+	protected Map<SDGNode, L> initCL(boolean incorporateSourceAnns) {
+		Map<SDGNode, L> ret = new HashMap<SDGNode, L>();
+		for (SDGNode n : sdg.vertexSet()) {
+			if (incorporateSourceAnns && srcAnn.containsKey(n)) {
+				ret.put(n, srcAnn.get(n));
+			} else {
+				ret.put(n, secLattice.getBottom());
+			}
+		}
+		return ret;
+	}
+
+	protected Map<Pair<SDGNode,SDGNode>, L> initCLT() {
+		Map<Pair<SDGNode,SDGNode>, L> ret = new HashMap<>();
+		for (SDGNode n : icfg.vertexSet()) {
+			for (SDGNode m : icfg.vertexSet()) {
+				if (mhp.isParallel(m, n)) ret.put(Pair.pair(n,m), secLattice.getBottom());
+			}
+		}
+		return ret;
+	}
+	
+	private boolean interferenceWriteUndirected(SDGNode n) {
+		for (SDGEdge e : sdg.getOutgoingEdgesOfKind(n, SDGEdge.Kind.INTERFERENCE_WRITE) ) {
+			boolean found = false;
+			for (SDGEdge e2 : sdg.getOutgoingEdgesOfKind(e.getTarget(), SDGEdge.Kind.INTERFERENCE_WRITE)) {
+				if (e2.getTarget().equals(n)) found = true;
+			}
+			return found;
+		}
+		return true;
+	}
+	
+	public int check() {
+		I2PBackward backw = new I2PBackward(sdg);
+		// 1.) initialize classification: we go from the bottom up, so every node is classified as low initially
+		// except for the sources: They are classified with the user-annotated source level
+		cl  = initCL(true);
+		clt = initCLT(); 
+		// 2.) fixed-point iteration
+		int numIters = 0;
+		boolean change;
+		do {
+			change = false;
+			for (SDGNode n : sdg.vertexSet()) {
+				L oldLevel = cl.get(n);
+				// nothing changes if current level is top already
+				if (secLattice.getTop().equals(oldLevel)) continue;
+				L newLevel = oldLevel;
+				// 2a.) propagate from backward slice
+				System.out.println(String.format("BS(%s) = %s", n, backw.slice(n)));
+				for (SDGNode m : backw.slice(n)) {
+					newLevel = secLattice.leastUpperBound(newLevel, cl.get(m));
+					if (secLattice.getTop().equals(newLevel)) {
+						break; // we can abort the loop here - level cannot get any higher
+					}
+				}
+				
+				// 2b.) propagate security levels from the relative timing of
+				//      i)  n, and
+				//      ii) nodes m from other threads whose execution write some value used at n
+				//          (i.e.: there is an interference dependence from m to n, or
+				//           a interference-write dependence between n and m)
+				assert(interferenceWriteUndirected(n));
+				
+				for (SDGEdge e : sdg.getIncomingEdgesOfKind(n, SDGEdge.Kind.INTERFERENCE)) {
+					final SDGNode m = e.getSource();
+					newLevel = secLattice.leastUpperBound(newLevel, clt.get(Pair.pair(n, m)));
+					if (secLattice.getTop().equals(newLevel)) {
+						break; // we can abort the loop here - level cannot get any higher
+					}
+				}
+				for (SDGEdge e : sdg.getIncomingEdgesOfKind(n, SDGEdge.Kind.INTERFERENCE_WRITE)) {
+					final SDGNode m = e.getSource();
+					newLevel = secLattice.leastUpperBound(newLevel, clt.get(Pair.pair(n, m)));
+					if (secLattice.getTop().equals(newLevel)) {
+						break; // we can abort the loop here - level cannot get any higher
+					}
+				}
+				if (!newLevel.equals(oldLevel)) {
+					cl.put(n, newLevel);
+					change = true;
+				}
+			}
+			
+			for (Pair<SDGNode,SDGNode> nm : clt.keySet()) {
+				L oldLevel = clt.get(nm);
+				// nothing changes if current level is top already
+				if (secLattice.getTop().equals(oldLevel)) continue;
+				L newLevel = oldLevel;
+				
+				final SDGNode n = nm.getFirst();
+				final SDGNode m = nm.getSecond();
+				
+				transClosure.computeIfAbsent(n, tcfgForwardSlicer::slice);
+				transClosure.computeIfAbsent(m, tcfgForwardSlicer::slice);
+				
+				for (int threadN : n.getThreadNumbers()) {
+					for (int threadM : m.getThreadNumbers()) {
+						final SDGNode c = cdomOracle.cdom(n, threadN ,m, threadM).getNode();
+						chops.computeIfAbsent(
+							Pair.pair(c, n),
+							pair -> tcfgChopper.chop(pair.getFirst(), pair.getSecond())
+						);
+						chops.computeIfAbsent(
+								Pair.pair(c, m),
+								pair -> tcfgChopper.chop(pair.getFirst(), pair.getSecond())
+						);
+						List<? extends SDGNode> relevant = Stream.concat(
+							chops.get(Pair.pair(c,n)).stream(),
+							timingDependence.get(n).stream()
+						).collect(Collectors.toList());
+						for (SDGNode c2 : relevant) {
+							newLevel = secLattice.leastUpperBound(newLevel, cl.get(c2));
+							if (secLattice.getTop().equals(newLevel)) {
+								break; // we can abort the loop here - level cannot get any higher
+							}
+						}
+					}
+				}
+				if (!newLevel.equals(oldLevel)) {
+					clt.put(nm, newLevel);
+					change = true;
+				}
+				
+
+				
+			}
+			
+			numIters++;
+		} while (change);
+		System.out.println(String.format("needed %d iteration(s).", numIters));
+		// 3.) check that sink levels comply
+		return checkCompliance();
+	}
+
+	protected final int checkCompliance() {
+		boolean compliant = true;
+		int noViolations = 0;
+		for (Map.Entry<SDGNode, L> snkEntry : snkAnn.entrySet()) {
+			SDGNode s = snkEntry.getKey();
+			L snkLevel = snkEntry.getValue();
+			if (!LatticeUtil.isLeq(secLattice, cl.get(s), snkLevel)) {
+				System.out.println("Violation at node " + s + ": user-annotated level is " + snkLevel + ", computed level is " + cl.get(s));
+				noViolations++;
+				compliant = false;
+			}
+		}
+		for (Pair<SDGNode,SDGNode> nm : clt.keySet()) {
+			final SDGNode n = nm.getFirst();
+			final SDGNode m = nm.getSecond();
+			if(snkAnn.containsKey(n) && snkAnn.containsKey(m) && (
+			      !LatticeUtil.isLeq(secLattice, clt.get(nm), snkAnn.get(n))
+			   || !LatticeUtil.isLeq(secLattice, clt.get(nm), snkAnn.get(m)))
+			){
+				System.out.println("Violation at nodes " + nm + ": user-annotated levels are (" +
+			                       snkAnn.get(n) + ", "+ snkAnn.get(m) + "), " +
+				                   "but their relative timing is classified " + clt.get(nm));
+				noViolations++;
+				compliant = false;
+			}
+		}
+		if (compliant) {
+			System.out.println("no violations found.");
+		}
+		return noViolations;
+	}
+}
---
+++ src/tests/ORLSODExperimentTiming.java
@@ -0,0 +1,112 @@
+package tests;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.ibm.wala.ipa.cha.ClassHierarchyException;
+import com.ibm.wala.util.CancelException;
+import com.ibm.wala.util.graph.GraphIntegrity.UnsoundGraphException;
+
+import edu.kit.joana.api.lattice.BuiltinLattices;
+import edu.kit.joana.ifc.orlsod.ClassicCDomOracle;
+import edu.kit.joana.ifc.orlsod.ORLSODChecker;
+import edu.kit.joana.ifc.orlsod.PathBasedORLSODChecker;
+import edu.kit.joana.ifc.orlsod.ProbInfComputer;
+import edu.kit.joana.ifc.orlsod.ThreadModularCDomOracle;
+import edu.kit.joana.ifc.orlsod.TimimgClassificationChecker;
+import edu.kit.joana.ifc.sdg.graph.SDG;
+import edu.kit.joana.ifc.sdg.graph.SDGEdge;
+import edu.kit.joana.ifc.sdg.graph.SDGNode;
+import edu.kit.joana.ifc.sdg.graph.SDGSerializer;
+import edu.kit.joana.ifc.sdg.graph.slicer.graph.CFG;
+import edu.kit.joana.ifc.sdg.graph.slicer.graph.threads.PreciseMHPAnalysis;
+import edu.kit.joana.ifc.sdg.mhpoptimization.CSDGPreprocessor;
+import edu.kit.joana.ifc.sdg.mhpoptimization.PruneInterferences;
+import edu.kit.joana.ifc.sdg.util.BytecodeLocation;
+import edu.kit.joana.ifc.sdg.util.sdg.ReducedCFGBuilder;
+import edu.kit.joana.util.Util;
+
+public class ORLSODExperimentTiming {
+
+	@Test
+	public void doORLSOD1() throws ClassHierarchyException, IOException, UnsoundGraphException, CancelException {
+		doConfigTiming(new StandardTestConfig("example/bin", "Lorlsod/ORLSOD1", "orlsod1", 1, 2, 2));
+	}
+
+	@Test
+	public void doORLSOD2() throws ClassHierarchyException, IOException, UnsoundGraphException, CancelException {
+		doConfigTiming(new StandardTestConfig("example/bin", "Lorlsod/ORLSOD2", "orlsod2", 1, 2, 0));
+	}
+
+	@Test
+	public void doORLSOD3() throws ClassHierarchyException, IOException, UnsoundGraphException, CancelException {
+		doConfigTiming(new StandardTestConfig("example/bin", "Lorlsod/ORLSOD3", "orlsod3", 1, 2, 0));
+	}
+	@Test
+	public void doNoSecret() throws ClassHierarchyException, IOException, UnsoundGraphException, CancelException {
+		doConfigTiming(new StandardTestConfig("example/bin", "Lorlsod/NoSecret", "noSecret", 0, 2, 0));
+	}
+
+	@Test
+	public void doLateSecretAccess() throws ClassHierarchyException, IOException, UnsoundGraphException, CancelException {
+		doConfigTiming(new StandardTestConfig("example/bin", "Lorlsod/LateSecretAccess", "lateSecAccess", 1, 2, 0));
+	}
+
+	
+	private static void doConfigTiming(TestConfig cfg) throws ClassHierarchyException, IOException, UnsoundGraphException, CancelException {
+		SDG sdg = JoanaRunner.buildSDG(cfg.progDesc.classPath, cfg.progDesc.mainClass);
+		CSDGPreprocessor.preprocessSDG(sdg);
+		CFG redCFG = ReducedCFGBuilder.extractReducedCFG(sdg);
+		Util.removeCallCallRetEdges(redCFG);
+		DomExperiment.export(redCFG, DomExperiment.joanaGraphExporter(), cfg.outputFiles.dotFile);
+		PreciseMHPAnalysis mhp = PreciseMHPAnalysis.analyze(sdg);
+		PruneInterferences.pruneInterferences(sdg, mhp);
+		PrintWriter pw = new PrintWriter(cfg.outputFiles.pdgFile);
+		SDGSerializer.toPDGFormat(sdg, pw);
+		pw.close();
+		Map<SDGNode, String> srcAnn = new HashMap<SDGNode, String>();
+		for (SDGNode src : cfg.srcSelector.select(sdg)) {
+			srcAnn.put(src, BuiltinLattices.STD_SECLEVEL_HIGH);
+			System.out.println(String.format("srcAnn(%s) = %s", src, BuiltinLattices.STD_SECLEVEL_HIGH));
+		}
+		Assert.assertEquals(cfg.expectedNoSources, srcAnn.keySet().size());
+		Map<SDGNode, String> snkAnn = new HashMap<SDGNode, String>();
+		for (SDGNode snk : cfg.snkSelector.select(sdg)) {
+			snkAnn.put(snk, BuiltinLattices.STD_SECLEVEL_LOW);
+			System.out.println(String.format("snkAnn(%s) = %s", snk, BuiltinLattices.STD_SECLEVEL_LOW));
+		}
+		Assert.assertEquals(cfg.expectedNoSinks, snkAnn.keySet().size());
+		ThreadModularCDomOracle tmdo = new ThreadModularCDomOracle(sdg);
+		ProbInfComputer probInf = new ProbInfComputer(sdg, tmdo);
+		TimimgClassificationChecker<String> checker = new TimimgClassificationChecker<>(sdg, BuiltinLattices.getBinaryLattice(), srcAnn, snkAnn, probInf, mhp, tmdo);
+		int noVios = checker.check();
+		Assert.assertEquals(cfg.expectedNoViolations, noVios);
+	}
+
+	@Test
+	public void testORLSOD5a() throws ClassHierarchyException, IOException, UnsoundGraphException, CancelException {
+		doConfigTiming(new StandardTestConfig("example/bin", "Lorlsod/ORLSOD5a", "orlsod5a", 1, 2, 6));
+	}
+
+	@Test
+	public void testORLSODSecure() throws ClassHierarchyException, IOException, UnsoundGraphException, CancelException {
+		doConfigTiming(new StandardTestConfig("example/bin", "Lorlsod/ORLSOD5Secure", "orlsod5secure", 1, 2, 0));
+	}
+
+	
+	@Test
+	public void testPost_Fig2_3() throws ClassHierarchyException, IOException, UnsoundGraphException, CancelException {
+		doConfigTiming(new StandardTestConfig("example/bin", "Lpost16/Fig2_3", "post_fig2_3", 1, 2, 2));
+	}
+
+}

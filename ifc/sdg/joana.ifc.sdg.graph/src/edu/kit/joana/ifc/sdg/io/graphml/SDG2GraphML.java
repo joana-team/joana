39285eebd66f8da788a48516031e42cab0997644
Martin Hecker <martin.hecker@kit.edu>
2016-05-09 17:54:29 +0200

    refactorisation of string constants in graphml export

--- ifc/sdg/joana.ifc.sdg.graph/src/edu/kit/joana/ifc/sdg/io/graphml/SDG2GraphML.java
+++ ifc/sdg/joana.ifc.sdg.graph/src/edu/kit/joana/ifc/sdg/io/graphml/SDG2GraphML.java
@@ -25,66 +25,104 @@ import javanet.staxutils.IndentingXMLStreamWriter;
  * @author Martin Hecker <martin.hecker@kit.edu>
  */
 public class SDG2GraphML {
+	/**
+	 * Constants for names of JOANA specific node- and edge-attributes
+	 */
+	private static final String JOANA_EDGE_LABEL = "edgeLabel";
+	private static final String JOANA_EDGE_KIND = "edgeKind";
+	private static final String JOANA_NODE_EC = "nodeEc";
+	private static final String JOANA_NODE_ER = "nodeEr";
+	private static final String JOANA_NODE_SC = "nodeSc";
+	private static final String JOANA_NODE_SR = "nodeSr";
+	private static final String JOANA_NODE_BC_INDEX = "nodeBCIndex";
+	private static final String JOANA_NODE_BC_NAME = "nodeBcName";
+	private static final String JOANA_NODE_LABEL = "nodeLabel";
+	private static final String JOANA_NODE_OPERATION = "nodeOperation";
+	private static final String JOANA_NODE_PROC = "nodeProc";
+	private static final String JOANA_NODE_SOURCE = "nodeSource";
+	private static final String JOANA_NODE_KIND = "nodeKind";
+	
+	/**
+	 * Constants for names from the graphml namespace
+	 */
+	private static final String GRAPHML_DIRECTED = "directed";
+	private static final String GRAPHML_EDGEDEFAULT = "edgedefault";
+	private static final String GRAPHML_GRAPH = "graph";
+	private static final String GRAPHML_GRAPHML = "graphml";
+	private static final String GRAPHML_XMLNS = "http://graphml.graphdrawing.org/xmlns";
+	private static final String GRAPHML_INT = "int";
+	private static final String GRAPHML_STRING = "string";
+	private static final String GRAPHML_ATTR_TYPE = "attr.type";
+	private static final String GRAPHML_ATTR_NAME = "attr.name";
+	private static final String GRAPHML_FOR = "for";
+	private static final String GRAPHML_SOURCE = "source";
+	private static final String GRAPHML_EDGE = "edge";
+	private static final String GRAPHML_KEY = "key";
+	private static final String GRAPHML_DATA = "data";
+	private static final String GRAPHML_ID = "id";
+	private static final String GRAPHML_NODE = "node";
+	
+	
 	private static final boolean OUTPUT_EDGELABELS=false;
 	private static final String ENCODING = "UTF-8";
 	
 	private static void writeNode(SDGNode n, XMLStreamWriter writer) throws XMLStreamException {
-		writer.writeStartElement("node"); {
-			writer.writeAttribute("id", Integer.toString(n.getId()));
-			writer.writeStartElement("data"); {
-				writer.writeAttribute("key", "nodeKind");
+		writer.writeStartElement(GRAPHML_NODE); {
+			writer.writeAttribute(GRAPHML_ID, Integer.toString(n.getId()));
+			writer.writeStartElement(GRAPHML_DATA); {
+				writer.writeAttribute(GRAPHML_KEY, JOANA_NODE_KIND);
 				writer.writeCharacters(n.getKind().toString());
 			} writer.writeEndElement();
 			
-			writer.writeStartElement("data"); {
-				writer.writeAttribute("key", "nodeSource");
+			writer.writeStartElement(GRAPHML_DATA); {
+				writer.writeAttribute(GRAPHML_KEY, JOANA_NODE_SOURCE);
 				writer.writeCharacters(n.getSource());
 			} writer.writeEndElement();
 			
-			writer.writeStartElement("data"); {
-				writer.writeAttribute("key", "nodeProc");
+			writer.writeStartElement(GRAPHML_DATA); {
+				writer.writeAttribute(GRAPHML_KEY, JOANA_NODE_PROC);
 				writer.writeCharacters(Integer.toString(n.getProc()));
 			} writer.writeEndElement();
 			
-			writer.writeStartElement("data"); {
-				writer.writeAttribute("key", "nodeOperation");
+			writer.writeStartElement(GRAPHML_DATA); {
+				writer.writeAttribute(GRAPHML_KEY, JOANA_NODE_OPERATION);
 				writer.writeCharacters(n.getOperation().toString());
 			} writer.writeEndElement();
 			
-			writer.writeStartElement("data"); {
-				writer.writeAttribute("key", "nodeLabel");
+			writer.writeStartElement(GRAPHML_DATA); {
+				writer.writeAttribute(GRAPHML_KEY, JOANA_NODE_LABEL);
 				writer.writeCharacters(n.getLabel());
 			} writer.writeEndElement();
 			
-			writer.writeStartElement("data"); {
-				writer.writeAttribute("key", "nodeBcName");
+			writer.writeStartElement(GRAPHML_DATA); {
+				writer.writeAttribute(GRAPHML_KEY, JOANA_NODE_BC_NAME);
 				writer.writeCharacters(n.getBytecodeName());
 			} writer.writeEndElement();
 			
-			writer.writeStartElement("data"); {
-				writer.writeAttribute("key", "nodeBCIndex");
+			writer.writeStartElement(GRAPHML_DATA); {
+				writer.writeAttribute(GRAPHML_KEY, JOANA_NODE_BC_INDEX);
 				writer.writeCharacters(Integer.toString(n.getBytecodeIndex()));
 			} writer.writeEndElement();
 			
-			writer.writeStartElement("data"); {
-				writer.writeAttribute("key", "nodeBCIndex");
+			writer.writeStartElement(GRAPHML_DATA); {
+				writer.writeAttribute(GRAPHML_KEY, JOANA_NODE_BC_INDEX);
 				writer.writeCharacters(Integer.toString(n.getBytecodeIndex()));
 			} writer.writeEndElement();
 			
-			writer.writeStartElement("data"); {
-				writer.writeAttribute("key", "nodeSr");
+			writer.writeStartElement(GRAPHML_DATA); {
+				writer.writeAttribute(GRAPHML_KEY, JOANA_NODE_SR);
 				writer.writeCharacters(Integer.toString(n.getSr()));
 			} writer.writeEndElement();
-			writer.writeStartElement("data"); {
-				writer.writeAttribute("key", "nodeSc");
+			writer.writeStartElement(GRAPHML_DATA); {
+				writer.writeAttribute(GRAPHML_KEY, JOANA_NODE_SC);
 				writer.writeCharacters(Integer.toString(n.getSc()));
 			} writer.writeEndElement();
-			writer.writeStartElement("data"); {
-				writer.writeAttribute("key", "nodeEr");
+			writer.writeStartElement(GRAPHML_DATA); {
+				writer.writeAttribute(GRAPHML_KEY, JOANA_NODE_ER);
 				writer.writeCharacters(Integer.toString(n.getEr()));
 			} writer.writeEndElement();
-			writer.writeStartElement("data"); {
-				writer.writeAttribute("key", "nodeEc");
+			writer.writeStartElement(GRAPHML_DATA); {
+				writer.writeAttribute(GRAPHML_KEY, JOANA_NODE_EC);
 				writer.writeCharacters(Integer.toString(n.getEc()));
 			} writer.writeEndElement();
 			
@@ -92,18 +130,18 @@ public class SDG2GraphML {
 	}
 	
 	private static void writeEdge(SDGEdge e, XMLStreamWriter writer) throws XMLStreamException {
-		writer.writeStartElement("edge"); {
-			writer.writeAttribute("source", Integer.toString(e.getSource().getId()));
+		writer.writeStartElement(GRAPHML_EDGE); {
+			writer.writeAttribute(GRAPHML_SOURCE, Integer.toString(e.getSource().getId()));
 			writer.writeAttribute("target", Integer.toString(e.getTarget().getId()));
 			
-			writer.writeStartElement("data"); {
-				writer.writeAttribute("key", "edgeKind");
+			writer.writeStartElement(GRAPHML_DATA); {
+				writer.writeAttribute(GRAPHML_KEY, JOANA_EDGE_KIND);
 				writer.writeCharacters(e.getKind().toString());
 			} writer.writeEndElement();
 			
 			if (OUTPUT_EDGELABELS) { 
-				writer.writeStartElement("data"); {
-					writer.writeAttribute("key", "edgeLabel");
+				writer.writeStartElement(GRAPHML_DATA); {
+					writer.writeAttribute(GRAPHML_KEY, JOANA_EDGE_LABEL);
 					writer.writeCharacters(e.getLabel());
 				} writer.writeEndElement();
 			}
@@ -112,112 +150,112 @@ public class SDG2GraphML {
 	}
 	
 	private static void writeKeyDeclarations(XMLStreamWriter writer) throws XMLStreamException {
-		writer.writeEmptyElement("key"); {
-			writer.writeAttribute("id", "nodeKind");
-			writer.writeAttribute("for", "node");
-			writer.writeAttribute("attr.name", "nodeKind");
-			writer.writeAttribute("attr.type", "string");
+		writer.writeEmptyElement(GRAPHML_KEY); {
+			writer.writeAttribute(GRAPHML_ID, JOANA_NODE_KIND);
+			writer.writeAttribute(GRAPHML_FOR, GRAPHML_NODE);
+			writer.writeAttribute(GRAPHML_ATTR_NAME, JOANA_NODE_KIND);
+			writer.writeAttribute(GRAPHML_ATTR_TYPE, GRAPHML_STRING);
 		}
 		
-		writer.writeEmptyElement("key"); {
-			writer.writeAttribute("id", "nodeSource");
-			writer.writeAttribute("for", "node");
-			writer.writeAttribute("attr.name", "nodeSource");
-			writer.writeAttribute("attr.type", "string");
+		writer.writeEmptyElement(GRAPHML_KEY); {
+			writer.writeAttribute(GRAPHML_ID, JOANA_NODE_SOURCE);
+			writer.writeAttribute(GRAPHML_FOR, GRAPHML_NODE);
+			writer.writeAttribute(GRAPHML_ATTR_NAME, JOANA_NODE_SOURCE);
+			writer.writeAttribute(GRAPHML_ATTR_TYPE, GRAPHML_STRING);
 		}
 		
-		writer.writeEmptyElement("key"); {
-			writer.writeAttribute("id", "nodeProc");
-			writer.writeAttribute("for", "node");
-			writer.writeAttribute("attr.name", "nodeProc");
-			writer.writeAttribute("attr.type", "int");
+		writer.writeEmptyElement(GRAPHML_KEY); {
+			writer.writeAttribute(GRAPHML_ID, JOANA_NODE_PROC);
+			writer.writeAttribute(GRAPHML_FOR, GRAPHML_NODE);
+			writer.writeAttribute(GRAPHML_ATTR_NAME, JOANA_NODE_PROC);
+			writer.writeAttribute(GRAPHML_ATTR_TYPE, GRAPHML_INT);
 		}
 		
-		writer.writeEmptyElement("key"); {
-			writer.writeAttribute("id", "nodeOperation");
-			writer.writeAttribute("for", "node");
-			writer.writeAttribute("attr.name", "nodeOperation");
-			writer.writeAttribute("attr.type", "string");
+		writer.writeEmptyElement(GRAPHML_KEY); {
+			writer.writeAttribute(GRAPHML_ID, JOANA_NODE_OPERATION);
+			writer.writeAttribute(GRAPHML_FOR, GRAPHML_NODE);
+			writer.writeAttribute(GRAPHML_ATTR_NAME, JOANA_NODE_OPERATION);
+			writer.writeAttribute(GRAPHML_ATTR_TYPE, GRAPHML_STRING);
 		}
 		
-		writer.writeEmptyElement("key"); {
-			writer.writeAttribute("id", "nodeLabel");
-			writer.writeAttribute("for", "node");
-			writer.writeAttribute("attr.name", "nodeLabel");
-			writer.writeAttribute("attr.type", "string");
+		writer.writeEmptyElement(GRAPHML_KEY); {
+			writer.writeAttribute(GRAPHML_ID, JOANA_NODE_LABEL);
+			writer.writeAttribute(GRAPHML_FOR, GRAPHML_NODE);
+			writer.writeAttribute(GRAPHML_ATTR_NAME, JOANA_NODE_LABEL);
+			writer.writeAttribute(GRAPHML_ATTR_TYPE, GRAPHML_STRING);
 		}
 		
-		writer.writeEmptyElement("key"); {
-			writer.writeAttribute("id", "nodeBcName");
-			writer.writeAttribute("for", "node");
-			writer.writeAttribute("attr.name", "nodeBcName");
-			writer.writeAttribute("attr.type", "string");
+		writer.writeEmptyElement(GRAPHML_KEY); {
+			writer.writeAttribute(GRAPHML_ID, JOANA_NODE_BC_NAME);
+			writer.writeAttribute(GRAPHML_FOR, GRAPHML_NODE);
+			writer.writeAttribute(GRAPHML_ATTR_NAME, JOANA_NODE_BC_NAME);
+			writer.writeAttribute(GRAPHML_ATTR_TYPE, GRAPHML_STRING);
 		}
 		
-		writer.writeEmptyElement("key"); {
-			writer.writeAttribute("id", "nodeBCIndex");
-			writer.writeAttribute("for", "node");
-			writer.writeAttribute("attr.name", "nodeBCIndex");
-			writer.writeAttribute("attr.type", "int");
+		writer.writeEmptyElement(GRAPHML_KEY); {
+			writer.writeAttribute(GRAPHML_ID, JOANA_NODE_BC_INDEX);
+			writer.writeAttribute(GRAPHML_FOR, GRAPHML_NODE);
+			writer.writeAttribute(GRAPHML_ATTR_NAME, JOANA_NODE_BC_INDEX);
+			writer.writeAttribute(GRAPHML_ATTR_TYPE, GRAPHML_INT);
 		}
 		
-		writer.writeEmptyElement("key"); {
-			writer.writeAttribute("id", "nodeSr");
-			writer.writeAttribute("for", "node");
-			writer.writeAttribute("attr.name", "nodeSr");
-			writer.writeAttribute("attr.type", "int");
+		writer.writeEmptyElement(GRAPHML_KEY); {
+			writer.writeAttribute(GRAPHML_ID, JOANA_NODE_SR);
+			writer.writeAttribute(GRAPHML_FOR, GRAPHML_NODE);
+			writer.writeAttribute(GRAPHML_ATTR_NAME, JOANA_NODE_SR);
+			writer.writeAttribute(GRAPHML_ATTR_TYPE, GRAPHML_INT);
 		}
 		
-		writer.writeEmptyElement("key"); {
-			writer.writeAttribute("id", "nodeSc");
-			writer.writeAttribute("for", "node");
-			writer.writeAttribute("attr.name", "nodeSc");
-			writer.writeAttribute("attr.type", "int");
+		writer.writeEmptyElement(GRAPHML_KEY); {
+			writer.writeAttribute(GRAPHML_ID, JOANA_NODE_SC);
+			writer.writeAttribute(GRAPHML_FOR, GRAPHML_NODE);
+			writer.writeAttribute(GRAPHML_ATTR_NAME, JOANA_NODE_SC);
+			writer.writeAttribute(GRAPHML_ATTR_TYPE, GRAPHML_INT);
 		}
 		
-		writer.writeEmptyElement("key"); {
-			writer.writeAttribute("id", "nodeEr");
-			writer.writeAttribute("for", "node");
-			writer.writeAttribute("attr.name", "nodeEr");
-			writer.writeAttribute("attr.type", "int");
+		writer.writeEmptyElement(GRAPHML_KEY); {
+			writer.writeAttribute(GRAPHML_ID, JOANA_NODE_ER);
+			writer.writeAttribute(GRAPHML_FOR, GRAPHML_NODE);
+			writer.writeAttribute(GRAPHML_ATTR_NAME, JOANA_NODE_ER);
+			writer.writeAttribute(GRAPHML_ATTR_TYPE, GRAPHML_INT);
 		}
 		
-		writer.writeEmptyElement("key"); {
-			writer.writeAttribute("id", "nodeEc");
-			writer.writeAttribute("for", "node");
-			writer.writeAttribute("attr.name", "nodeEc");
-			writer.writeAttribute("attr.type", "int");
+		writer.writeEmptyElement(GRAPHML_KEY); {
+			writer.writeAttribute(GRAPHML_ID, JOANA_NODE_EC);
+			writer.writeAttribute(GRAPHML_FOR, GRAPHML_NODE);
+			writer.writeAttribute(GRAPHML_ATTR_NAME, JOANA_NODE_EC);
+			writer.writeAttribute(GRAPHML_ATTR_TYPE, GRAPHML_INT);
 		}
 		
 		
-		writer.writeEmptyElement("key"); {
-			writer.writeAttribute("id", "edgeKind");
-			writer.writeAttribute("for", "node");
-			writer.writeAttribute("attr.name", "edgeKind");
-			writer.writeAttribute("attr.type", "string");
+		writer.writeEmptyElement(GRAPHML_KEY); {
+			writer.writeAttribute(GRAPHML_ID, JOANA_EDGE_KIND);
+			writer.writeAttribute(GRAPHML_FOR, GRAPHML_NODE);
+			writer.writeAttribute(GRAPHML_ATTR_NAME, JOANA_EDGE_KIND);
+			writer.writeAttribute(GRAPHML_ATTR_TYPE, GRAPHML_STRING);
 		}
 		
-		writer.writeEmptyElement("key"); {
-			writer.writeAttribute("id", "edgeKind");
-			writer.writeAttribute("for", "node");
-			writer.writeAttribute("attr.name", "edgeKind");
-			writer.writeAttribute("attr.type", "string");
+		writer.writeEmptyElement(GRAPHML_KEY); {
+			writer.writeAttribute(GRAPHML_ID, JOANA_EDGE_KIND);
+			writer.writeAttribute(GRAPHML_FOR, GRAPHML_NODE);
+			writer.writeAttribute(GRAPHML_ATTR_NAME, JOANA_EDGE_KIND);
+			writer.writeAttribute(GRAPHML_ATTR_TYPE, GRAPHML_STRING);
 		}
-		writer.writeEmptyElement("key"); {
-			writer.writeAttribute("id", "edgeLabel");
-			writer.writeAttribute("for", "node");
-			writer.writeAttribute("attr.name", "edgeLabel");
-			writer.writeAttribute("attr.type", "string");
+		writer.writeEmptyElement(GRAPHML_KEY); {
+			writer.writeAttribute(GRAPHML_ID, JOANA_EDGE_LABEL);
+			writer.writeAttribute(GRAPHML_FOR, GRAPHML_NODE);
+			writer.writeAttribute(GRAPHML_ATTR_NAME, JOANA_EDGE_LABEL);
+			writer.writeAttribute(GRAPHML_ATTR_TYPE, GRAPHML_STRING);
 		}
 	}
 	
 	private static void writeStartRoot(XMLStreamWriter writer) throws XMLStreamException {
 		writer.writeStartDocument(ENCODING, "1.0");
-		writer.setPrefix("gr", "http://graphml.graphdrawing.org/xmlns");
-		writer.setDefaultNamespace("http://graphml.graphdrawing.org/xmlns");
+		writer.setPrefix("gr", GRAPHML_XMLNS);
+		writer.setDefaultNamespace(GRAPHML_XMLNS);
 		
-		writer.writeStartElement("http://graphml.graphdrawing.org/xmlns", "graphml"); {
-			writer.writeDefaultNamespace("http://graphml.graphdrawing.org/xmlns");
+		writer.writeStartElement(GRAPHML_XMLNS, GRAPHML_GRAPHML); {
+			writer.writeDefaultNamespace(GRAPHML_XMLNS);
 			writer.writeNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
 			writer.writeAttribute("http://www.w3.org/2001/XMLSchema-instance", "schemaLocation", "http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd");
 		}
@@ -234,9 +272,9 @@ public class SDG2GraphML {
 		writeStartRoot(writer);
 		writeKeyDeclarations(writer);
 		
-		writer.writeStartElement("graph"); {
-			writer.writeAttribute("id", "G");
-			writer.writeAttribute("edgedefault", "directed");
+		writer.writeStartElement(GRAPHML_GRAPH); {
+			writer.writeAttribute(GRAPHML_ID, "G");
+			writer.writeAttribute(GRAPHML_EDGEDEFAULT, GRAPHML_DIRECTED);
 			
 			for (SDGNode n : sdg.vertexSet()) {
 				writeNode(n, writer);
@@ -258,20 +296,20 @@ public class SDG2GraphML {
 		writeStartRoot(writer);
 		writeKeyDeclarations(writer);
 		
-		writer.writeStartElement("graph"); {
-			writer.writeAttribute("id", "callgraph");
-			writer.writeAttribute("edgedefault", "directed");
+		writer.writeStartElement(GRAPHML_GRAPH); {
+			writer.writeAttribute(GRAPHML_ID, "callgraph");
+			writer.writeAttribute(GRAPHML_EDGEDEFAULT, GRAPHML_DIRECTED);
 			
 			for (Entry<SDGNode, Set<SDGNode>> entry : sdg.sortByProcedures().entrySet()) {
 				final SDGNode entryNode = entry.getKey();
 				final Set<SDGNode> nodes = entry.getValue();
-				writer.writeStartElement("node"); {
+				writer.writeStartElement(GRAPHML_NODE); {
 					// TODO: possibly find a nicer naming scheme?
-					writer.writeAttribute("id", "proc" + Integer.toString(entryNode.getId()));
+					writer.writeAttribute(GRAPHML_ID, "proc" + Integer.toString(entryNode.getId()));
 					
-					writer.writeStartElement("graph"); {
-						writer.writeAttribute("id", "proc" + Integer.toString(entryNode.getId()) + ":");
-						writer.writeAttribute("edgedefault", "directed");
+					writer.writeStartElement(GRAPHML_GRAPH); {
+						writer.writeAttribute(GRAPHML_ID, "proc" + Integer.toString(entryNode.getId()) + ":");
+						writer.writeAttribute(GRAPHML_EDGEDEFAULT, GRAPHML_DIRECTED);
 						
 						for (SDGNode n : nodes) {
 							writeNode(n, writer);

63ff7203d4066057f8d1cdbfeeb85e2d50fbc835
Martin Mohr <martin.mohr@kit.edu>
2016-08-30 17:25:42 +0200

    new pruning policy which lets Thread.run() live

---
+++ wala/joana.wala.core/src/edu/kit/joana/wala/core/ThreadAwareApplicationLoaderPolicy.java
@@ -0,0 +1,36 @@
+/**
+ * This file is part of the Joana IFC project. It is developed at the
+ * Programming Paradigms Group of the Karlsruhe Institute of Technology.
+ *
+ * For further details on licensing please read the information at
+ * http://joana.ipd.kit.edu or contact the authors.
+ */
+package edu.kit.joana.wala.core;
+
+import com.ibm.wala.ipa.callgraph.CGNode;
+import com.ibm.wala.ipa.callgraph.pruned.ApplicationLoaderPolicy;
+import com.ibm.wala.ipa.callgraph.pruned.PruningPolicy;
+import com.ibm.wala.ipa.cha.IClassHierarchy;
+
+import edu.kit.joana.wala.core.interference.ThreadInformationProvider;
+
+/**
+ * A pruning policy which behaves like {@link com.ibm.wala.ipa.callgraph.pruned.ApplicationLoaderPolicy} but does not prune
+ * away thread entries.
+ * @author Martin Mohr &lt;martin.mohr@kit.edu&gt;
+ */
+public class ThreadAwareApplicationLoaderPolicy implements PruningPolicy {
+	private final IClassHierarchy cha;
+
+	public ThreadAwareApplicationLoaderPolicy(IClassHierarchy cha) {
+		this.cha = cha;
+	}
+	/* (non-Javadoc)
+	 * @see com.ibm.wala.ipa.callgraph.pruned.PruningPolicy#check(com.ibm.wala.ipa.callgraph.CGNode)
+	 */
+	@Override
+	public boolean check(CGNode n) {
+		return ApplicationLoaderPolicy.INSTANCE.check(n) || ThreadInformationProvider.overwritesThreadRun(cha, n);
+	}
+
+}

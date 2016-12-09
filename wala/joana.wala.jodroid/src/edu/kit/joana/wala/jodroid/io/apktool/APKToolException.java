417bb3c525fff41df644c9001a412e005abf12f0
Martin Mohr <martin.mohr@kit.edu>
2016-12-09 19:19:05 +0100

    add code for conveniently analyzing android apps

---
+++ wala/joana.wala.jodroid/src/edu/kit/joana/wala/jodroid/io/apktool/APKToolException.java
@@ -0,0 +1,14 @@
+package edu.kit.joana.wala.jodroid.io.apktool;
+
+public class APKToolException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3438130718491968868L;
+	
+	public APKToolException(Throwable cause) {
+		super(cause);
+	}
+
+}

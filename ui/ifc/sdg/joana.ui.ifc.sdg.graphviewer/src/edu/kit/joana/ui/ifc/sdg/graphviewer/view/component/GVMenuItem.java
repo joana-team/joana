joana-team/joana

commit ac898070c93eedb9eea2dabf445e205208a4c595

more pmd suggestions

ui/ifc/sdg/joana.ui.ifc.sdg.graphviewer/src/edu/kit/joana/ui/ifc/sdg/graphviewer/view/component/GVMenuItem.java
@@ -69,50 +69,51 @@ public class GVMenuItem extends JMenuItem implements LanguageListener {
                 Action.ACTION_COMMAND_KEY, Action.ACCELERATOR_KEY, "enabled" };
         for (int i = 0; i < types.length; i++) {
             String type = types[i];
-            if (type.equals(Action.MNEMONIC_KEY)) {
+            
+            switch (type) {
+            case Action.MNEMONIC_KEY:
                 Integer n = (action == null) ? null : (Integer) action
                         .getValue(type);
                 this.setMnemonic(n == null ? '\0' : n.intValue());
-            } else if (type.equals(Action.NAME)) {
-                Boolean hide = (Boolean) this
-                        .getClientProperty("hideActionText");
-                this
-                        .setTextResource(action != null && hide != Boolean.TRUE ? (Resource) action
-                                .getValue(Action.NAME)
-                                : null);
-            } else if (type.equals(Action.SHORT_DESCRIPTION)) {
-                this.setToolTipResource(action != null ? (Resource) action
-                        .getValue(type) : null);
-            } else if (type.equals(Action.SMALL_ICON)) {
-                this.setIcon(action != null ? (Icon) action.getValue(type)
-                        : null);
-            } else if (type.equals(Action.ACTION_COMMAND_KEY)) {
-                this.setActionCommand(action != null ? (String) action
-                        .getValue(type) : null);
-            } else if (type.equals(Action.ACCELERATOR_KEY)) {
-                this.setAccelerator(action != null ? (KeyStroke) action
-                        .getValue(type) : null);
-            } else if (type.equals("enabled")) {
-                this.setEnabled(action != null ? action.isEnabled() : true);
+                break;
+            case Action.NAME:
+                Boolean hide = (Boolean) this.getClientProperty("hideActionText");
+                this.setTextResource(action == null || hide == Boolean.TRUE ? null
+                            : (Resource) action.getValue(Action.NAME));
+                break;
+            case Action.SHORT_DESCRIPTION:
+                this.setToolTipResource(action == null ? null : (Resource) action.getValue(type));
+                break;
+            case Action.SMALL_ICON:
+                this.setIcon(action == null ? null : (Icon) action.getValue(type));
+                break;
+            case Action.ACTION_COMMAND_KEY:
+                this.setActionCommand(action == null ? null : (String) action.getValue(type));
+                break;
+            case "enabled":
+                this.setEnabled(action == null || action.isEnabled());
+            	break;
+        	default:
+        		// no-op
             }
         }
     }
 
     public void setTextResource(Resource text) {
         this.text = text;
-        if (this.text != null) {
-            this.setText(this.translator.getString(text));
-        } else {
+        if (this.text == null) {
             this.setText(null);
+        } else {
+            this.setText(this.translator.getString(text));
         }
     }
 
     public void setToolTipResource(Resource toolTip) {
         this.toolTip = toolTip;
-        if (this.toolTip != null) {
-            this.setToolTipText(this.translator.getString(toolTip));
-        } else {
+        if (this.toolTip == null) {
             this.setToolTipText(null);
+        } else {
+            this.setToolTipText(this.translator.getString(toolTip));
         }
     }
 

commit a7bc11028fae99e5b90390ba3c29c4d1ac0e7a4e

fix bug in graphviewer introduced in ac898070c93ee where hotkeys didn't work anymore

ui/ifc/sdg/joana.ui.ifc.sdg.graphviewer/src/edu/kit/joana/ui/ifc/sdg/graphviewer/view/component/GVMenuItem.java
@@ -87,6 +87,9 @@ public class GVMenuItem extends JMenuItem implements LanguageListener {
             case Action.SMALL_ICON:
                 this.setIcon(action == null ? null : (Icon) action.getValue(type));
                 break;
+            case Action.ACCELERATOR_KEY:
+                this.setAccelerator(action == null ? null : (KeyStroke) action.getValue(type));
+                break;
             case Action.ACTION_COMMAND_KEY:
                 this.setActionCommand(action == null ? null : (String) action.getValue(type));
                 break;
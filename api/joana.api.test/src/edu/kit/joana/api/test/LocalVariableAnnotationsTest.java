2ab6520229926e6ace7212218d682e63dda254a5
Martin Hecker <martin.hecker@kit.edu>
2017-06-01 17:04:01 +0200

    some more test cases for local variable annotations

--- api/joana.api.test/src/edu/kit/joana/api/test/LocalVariableAnnotationsTest.java
+++ api/joana.api.test/src/edu/kit/joana/api/test/LocalVariableAnnotationsTest.java
@@ -8,6 +8,7 @@
 package edu.kit.joana.api.test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.util.Collection;
@@ -31,7 +32,7 @@ import edu.kit.joana.ifc.sdg.core.violations.IViolation;
 public class LocalVariableAnnotationsTest {
 
 
-	public void test(Class<?> cls) throws ClassHierarchyException, ApiTestException, IOException, UnsoundGraphException,
+	public void pass(Class<?> cls) throws ClassHierarchyException, ApiTestException, IOException, UnsoundGraphException,
 	CancelException {
 		IFCAnalysis ana = BuildSDG.buldAndUseJavaAnnotations(
 			cls,
@@ -43,13 +44,69 @@ public class LocalVariableAnnotationsTest {
 		Collection<? extends IViolation<SecurityNode>> illegal = ana.doIFC();
 		assertFalse(illegal.isEmpty());
 	}
+	
+	// tests that fail. Some programs, usually those that just pass around constants via several variables,
+	// just cannot be properly annotaed, because both in their IR and their PDGs there are no nodes that 
+	// sensible use and/or def those variables present.
+	public void fail(Class<?> cls) throws ClassHierarchyException, ApiTestException, IOException, UnsoundGraphException,
+	CancelException {
+		IFCAnalysis ana = BuildSDG.buldAndUseJavaAnnotations(
+			cls,
+			BuildSDG.top_sequential,
+			false);
+		DumpTestSDG.dumpSDG(ana.getProgram().getSDG(), cls.getCanonicalName() + ".pdg");
+		DumpTestSDG.dumpGraphML(ana.getProgram().getSDG(), cls.getCanonicalName() + ".pdg");
+
+		Collection<? extends IViolation<SecurityNode>> illegal = ana.doIFC();
+		assertTrue(illegal.isEmpty());
+	}
 
 	
 	@Test
 	public void test1() throws ClassHierarchyException, ApiTestException, IOException, UnsoundGraphException,
 	CancelException {
-		test(joana.api.testdata.javannotations.LocalVariableAnnotations1.class);
+		pass(joana.api.testdata.javannotations.LocalVariableAnnotations1.class);
 	}
+	
+	
+	@Test
+	public void test2() throws ClassHierarchyException, ApiTestException, IOException, UnsoundGraphException,
+	CancelException {
+		fail(joana.api.testdata.javannotations.LocalVariableAnnotations1.Test2.class);
+	}
+	
+	@Test
+	public void test3() throws ClassHierarchyException, ApiTestException, IOException, UnsoundGraphException,
+	CancelException {
+		fail(joana.api.testdata.javannotations.LocalVariableAnnotations1.Test3.class);
+	}
+	
+	@Test
+	public void test4() throws ClassHierarchyException, ApiTestException, IOException, UnsoundGraphException,
+	CancelException {
+		fail(joana.api.testdata.javannotations.LocalVariableAnnotations1.Test4.class);
+	}
+	
+	@Test
+	public void test22() throws ClassHierarchyException, ApiTestException, IOException, UnsoundGraphException,
+	CancelException {
+		fail(joana.api.testdata.javannotations.LocalVariableAnnotations1.Test22.class);
+	}
+	
+	@Test
+	public void test23() throws ClassHierarchyException, ApiTestException, IOException, UnsoundGraphException,
+	CancelException {
+		fail(joana.api.testdata.javannotations.LocalVariableAnnotations1.Test23.class);
+	}
+	
+	@Test
+	public void test24() throws ClassHierarchyException, ApiTestException, IOException, UnsoundGraphException,
+	CancelException {
+		fail(joana.api.testdata.javannotations.LocalVariableAnnotations1.Test24.class);
+	}
+	
+
+
 
 	public static void main(String[] args)
 		throws ClassHierarchyException,
--- api/joana.api.testdata/src/joana/api/testdata/javannotations/LocalVariableAnnotations1.java
+++ api/joana.api.testdata/src/joana/api/testdata/javannotations/LocalVariableAnnotations1.java
@@ -45,4 +45,113 @@ public class LocalVariableAnnotations1 {
 		rofl(foo(4,42));
 	}
 
+	public static int test22(int value) {
+		@Source(level=HIGH)
+		int x = value;
+		
+		int y = x;
+		
+		@Sink(level=LOW)
+		int z = y;
+		
+		return z;
+	}
+	
+	public static int test23(int value) {
+		@Source(level=HIGH)
+		int x = value;
+		
+		int y = x;
+		
+		@Sink(level=LOW)
+		int z = y;
+		
+		return 0;
+	}
+	
+	public static int test24(int value) {
+		@Source(level=HIGH)
+		int x = value;
+		
+		@Sink(level=LOW)
+		int z = x;
+		
+		return 0;
+	}
+	
+	
+	
+	
+	// failing tests
+	public static int test2() {
+		@Source(level=HIGH)
+		int x = 5;
+		
+		int y = x;
+		
+		@Sink(level=LOW)
+		int z = y;
+		
+		return z;
+	}
+	
+	public static int test3() {
+		@Source(level=HIGH)
+		int x = 5;
+		
+		int y = x;
+		
+		@Sink(level=LOW)
+		int z = y;
+		
+		return 0;
+	}
+	
+	public static int test4() {
+		@Source(level=HIGH)
+		int x = 5;
+		
+		@Sink(level=LOW)
+		int z = x;
+		
+		return 0;
+	}
+
+	
+	public static class Test2 {
+		public static void main(String[] args) {
+			test2();
+		}
+	}
+	
+	public static class Test3 {
+		public static void main(String[] args) {
+			test3();
+		}
+	}
+	
+	public static class Test4 {
+		public static void main(String[] args) {
+			test4();
+		}
+	}
+	
+	public static class Test22 {
+		public static void main(String[] args) {
+			test22(17);
+		}
+	}
+	
+	public static class Test23 {
+		public static void main(String[] args) {
+			test23(17);
+		}
+	}
+	
+	public static class Test24 {
+		public static void main(String[] args) {
+			test24(17);
+		}
+	}
+
 }

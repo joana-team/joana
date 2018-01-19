3f311859b18c0b019ef74d1dfc7842db6609db28
Martin Mohr <martin.mohr@kit.edu>
2018-01-19 18:04:47 +0100

    fix copy&paste error

--- util/joana.util/src/edu/kit/joana/util/Stubs.java
+++ util/joana.util/src/edu/kit/joana/util/Stubs.java
@@ -23,7 +23,7 @@ import joana.contrib.lib.Contrib;
  */
 public enum Stubs {
 	NO_STUBS("NONE", null), JRE_14_INCOMPLETE("JRE_14_INCOMPLETE", "stubs/jSDG-stubs-jre1.4.jar"), JRE_15_INCOMPLETE("JRE_15_INCOMPLETE", "stubs/jSDG-stubs-jre1.5.jar"),
-	JRE_15("JRE_15", "stubs/jdk-1.5-with-stubs.jar"), JRE_16("JRE_16", "stubs/jdk-1.6-with-stubs.jar"), JRE_17("JRE_16", "stubs/jdk-1.6-with-stubs.jar"),
+	JRE_15("JRE_15", "stubs/jdk-1.5-with-stubs.jar"), JRE_16("JRE_16", "stubs/jdk-1.6-with-stubs.jar"), JRE_17("JRE_17", "stubs/jdk-1.7-with-stubs.jar"),
 	JAVACARD("JAVACARD", "stubs/jSDG-stubs-javacard.jar:stubs/api.jar"),
 	J2ME("J2ME", "stubs/jSDG-stubs-j2me2.0.jar:stubs/jsr184.jar:stubs/mmapi.jar");
 

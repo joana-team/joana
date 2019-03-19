8f6cf37b62686702fbef337ec14436213287bb0c
Martin Hecker <martin.hecker@kit.edu>
2019-03-19 01:52:17 +0100

    alternative

--- wala/joana.wala.core/src/edu/kit/joana/wala/core/graphs/SinkpathPostDominators.java
+++ wala/joana.wala.core/src/edu/kit/joana/wala/core/graphs/SinkpathPostDominators.java
@@ -62,6 +62,7 @@ public class SinkpathPostDominators {
 		private boolean processed;
 		private boolean isSinkNode;
 		private boolean isRelevant;
+		private boolean changed;
 		
 		private Node<V> next;
 		private Node<V> representant;
@@ -73,6 +74,7 @@ public class SinkpathPostDominators {
 			this.processed = false;
 			this.isSinkNode = false;
 			this.isRelevant = false;
+			this.changed = false;
 			
 			this.representant = this;
 		}
@@ -279,4 +281,165 @@ public class SinkpathPostDominators {
 		
 		return result;
 	}
+
+	@SuppressWarnings("serial")
+	public static <V extends IntegerIdentifiable, E extends KnowsVertices<V>> DirectedGraph<Node<V>, ISinkdomEdge<Node<V>>> computeFixed(DirectedGraph<V, E> graph) {
+		
+		final Map<V, Node<V>> vToNode = new HashMap<>();
+		final AbstractJoanaGraph<Node<V>, ISinkdomEdge<Node<V>>> result; {
+			final ISinkdomEdge<Node<V>> dummy = new ISinkdomEdge<SinkpathPostDominators.Node<V>>(null, null);
+			@SuppressWarnings("unchecked")
+			final Class<ISinkdomEdge<Node<V>>> clazz = (Class<ISinkdomEdge<Node<V>>>) dummy.getClass();
+			result = new AbstractJoanaGraph<Node<V>, ISinkdomEdge<Node<V>>>(
+				new EdgeFactory<Node<V>, ISinkdomEdge<Node<V>>>() {
+					@Override
+					public ISinkdomEdge<Node<V>> createEdge(Node<V> sourceVertex, Node<V> targetVertex) {
+						return new ISinkdomEdge<>(sourceVertex, targetVertex); 
+					}
+				},
+				() -> new HashMap<>(graph.vertexSet().size()),
+				clazz
+			) {	};
+		}
+		
+		for (V v : graph.vertexSet()) {
+			final Node<V> n = new Node<V>(v);
+			vToNode.put(v, n);
+			result.addVertexUnsafe(n);
+		}
+		
+		final KosarajuStrongConnectivityInspector<V, E> sccInspector = new KosarajuStrongConnectivityInspector<V, E>(graph);
+		final List<Set<V>> sccs = sccInspector.stronglyConnectedSets();
+		
+		//final HashSet<Node<V>> relevant = new HashSet<>();
+		for (Set<V> scc : sccs) {
+			final boolean isSink = ! scc.stream().anyMatch(
+				v -> graph.outgoingEdgesOf(v).stream().anyMatch(
+					e -> !scc.contains(graph.getEdgeTarget(e))
+				)
+			);
+			if (isSink) {
+				Node<V> last = null;
+				Node<V> first = null;
+				for (V v : scc) {
+					final Node<V> n = vToNode.get(v);
+					if (last != null) {
+						newEdge(result, last, n);
+						processed(result, last);
+						last = n;
+					} else {
+						last = n;
+						first = n;
+					}
+					assert first != null;
+					n.representant = first;
+					n.isSinkNode = true;
+				}
+				if (last != first) newEdge(result, last, first); // TODO: pdf fixen
+				processed(result, last);
+			}
+		}
+		
+		final LinkedList<Node<V>> workqueue = new LinkedList<>();
+
+		for (Entry<V, Node<V>> entry : vToNode.entrySet()) {
+			final V v = entry.getKey();
+			final Node<V> x = entry.getValue();
+			if (!x.isSinkNode) {
+				final Set<E> successorEs = graph.outgoingEdgesOf(v);
+				switch (successorEs.size()) {
+					case 0: break;
+					case 1: {
+						final Node<V> z = vToNode.get(successorEs.iterator().next().getTarget());
+						if (z != x) {
+							newEdge(result, x, z.representant);
+							if (z.processed) processed(result, x); // TODO: pdf fixen
+						}
+						break;
+					}
+					default: {
+						x.isRelevant = true;
+						final ArrayList<Node<V>> successors = new ArrayList<>(successorEs.size());
+						for (E e : successorEs) {
+							successors.add(vToNode.get(e.getTarget()));
+						}
+						x.successors = successors;
+						workqueue.add(x);
+					}
+				}
+			}
+		}
+		
+		
+		{
+			while (!workqueue.isEmpty()) {
+				final Node<V> x = workqueue.removeFirst();
+				assert x.next == null && !x.processed;
+				final List<Node<V>> successors = x.successors;
+				final List<Node<V>> ys = successors.stream().filter(y -> y.processed).collect(Collectors.toList());
+				final Node<V> z;
+				if (ys.isEmpty()) {
+					z = null;
+				} else {
+					final Node<V> a = LeastCommonAncestor.lca(ys);
+					z = a == null ? ys.get(0) : a;
+				}
+				if (z != null) {
+					newEdge(result, x, z.representant);
+					processed(result, x);
+				} else {
+					workqueue.addLast(x);
+				}
+			}
+		}
+		{
+			final LinkedList<Node<V>> work  = new LinkedList<>();
+			
+			for (Node<V> n : vToNode.values()) {
+				if (n.next != null && n.isRelevant) work.add(n);
+			}
+			
+			ArrayList<Node<V>> workLeft  = new ArrayList<>(work.size());
+			ArrayList<Node<V>> workRight = new ArrayList<>(work.size());
+			workLeft.addAll(work);
+			work.clear();
+
+			
+			boolean changed;
+			do {
+				changed = false;
+				for (int i = 0; i < workLeft.size(); i++) {
+					final Node<V> x =  workLeft.get(i);
+					final List<Node<V>> successors = x.successors;
+					final Node<V> a = LeastCommonAncestor.lca(successors);
+					final Node<V> z = a == null ? null : a.representant;
+					assert x.next != null || z == null;
+					final boolean done = (z == null);
+					if (z != x.next) {
+						changed = true;
+						if (!x.changed) {
+							x.changed = true;
+							work.add(x);
+						}
+						
+						x.next = z;
+					}
+					if (!done) workRight.add(x);
+				}
+				workLeft  = workRight;
+				workRight = new ArrayList<>(workLeft.size());
+			} while (changed);
+			
+			for (Node<V> x : work) {
+				result.removeOutgoingEdgesOf(x);
+				final Node<V> z = x.next;
+				if (z != null) result.addEdgeUnsafe(x, z, new ISinkdomEdge<SinkpathPostDominators.Node<V>>(x, z));
+			}
+		}
+		
+		
+		
+		return result;
+	}
+
 }

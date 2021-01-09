2569669b32f7ef7ed948ba8b74fbbe1d6d0f8f5a
Tina Stroessner <stroessner.tinamaria@gmail.com>
2021-01-09 14:30:24 +0100

    get proper first example program

---
+++ ifc/sdg/qifc/joana.ifc.sdg.qifc.qif_interpreter/example/Simple.java
@@ -0,0 +1,16 @@
+public class Simple {
+
+
+	public  static void main(String[] args) {
+
+		Simple simple = new Simple();
+		simple.f(1);
+
+	}
+
+	public void f(int h) {
+		int l = 2;
+		l = l | h;
+		int __out = l;
+	}
+}

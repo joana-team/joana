772d1542aec2de2a5f8190625931b137ec970289
Tina Stroessner <stroessner.tinamaria@gmail.com>
2021-02-18 09:35:21 +0100

    update clean script to remove unneeded .class files

--- ifc/sdg/qifc/joana.ifc.sdg.qifc.qif_interpreter/clean.sh
+++ ifc/sdg/qifc/joana.ifc.sdg.qifc.qif_interpreter/clean.sh
@@ -2,7 +2,9 @@
 
 printf "Removing old output files + directories\n"
 find . -type d -name 'out_*' -exec rm -r {} \; > /dev/null 2>&1
-rm .attach_pid*
+find . -type f -path "./testResources/*" -name "*.class" -exec rm -f {} \;
+find . -type f -path "./examples/*" -name "*.class" -exec rm -f {} \;
+rm .attach_pid* > /dev/null 2>&1
 printf "Done\n\n"
 
 ant clean

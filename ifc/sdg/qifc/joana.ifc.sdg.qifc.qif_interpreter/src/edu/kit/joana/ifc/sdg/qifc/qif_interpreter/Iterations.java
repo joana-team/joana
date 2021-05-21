97c38e492677a8e5d99438aa68d7c90fccb7d0a6
Tina Stroessner <stroessner.tinamaria@gmail.com>
2021-05-21 15:21:55 +0200

    new testcase

---
+++ ifc/sdg/qifc/joana.ifc.sdg.qifc.qif_interpreter/src/edu/kit/joana/ifc/sdg/qifc/qif_interpreter/Iterations.java
@@ -0,0 +1,36 @@
+package edu.kit.joana.ifc.sdg.qifc.qif_interpreter;
+
+public class Iterations {
+
+	public static void main(String[] args) {
+
+		Iterations a = new Iterations();
+		a.f(4);
+
+	}
+
+	public int f(int h) {
+		int a = h;
+		int z = 1;
+		int f = 0b11111111111111111111111111111110;
+		int s = 0b11111111111111111111111111111101;
+		int t = 0b11111111111111111111111111111011;
+
+		while (a > 4) {
+			a = a - 1;
+			if (z == 1) {
+				h = h & f;
+			}
+			if (z == 2) {
+				h = h & s;
+			}
+			if (z == 3) {
+				h = h & t;
+			}
+			z = z + 1;
+			h = h & 0b11111111111111111111110000000000;
+		}
+		System.out.println(h);
+		return h;
+	}
+}

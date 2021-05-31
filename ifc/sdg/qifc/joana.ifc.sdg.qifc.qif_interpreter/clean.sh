1f3fe6c130ee89321c530b6f93aa2772503f5341
Tina Stroessner <stroessner.tinamaria@gmail.com>
2021-05-31 17:21:58 +0200

    update clean script

--- ifc/sdg/qifc/joana.ifc.sdg.qifc.qif_interpreter/clean.sh
+++ ifc/sdg/qifc/joana.ifc.sdg.qifc.qif_interpreter/clean.sh
@@ -3,7 +3,9 @@
 printf "Removing old output files + directories\n"
 find . -type d -name 'out_*' -exec rm -r {} \; >/dev/null 2>&1
 find . -type d -name 'test_*' -exec rm -r {} \; >/dev/null 2>&1
-rm *.dot *.cnf *.map *_out0
+rm *.dot
+rm *.cnf
+rm *.map
 find . -type f -path "./testResources/*" -name "*.class" -exec rm -f {} \;
 find . -type f -path "./examples/*" -name "*.class" -exec rm -f {} \;
 rm .attach_pid* >/dev/null 2>&1

8e4d73841b9bbff8fb49a567542b9b9e4cba474e
Tina Stroessner <stroessner.tinamaria@gmail.com>
2021-07-23 10:46:07 +0200

    fix paths

--- ifc/sdg/qifc/joana.ifc.sdg.qifc.qif_interpreter/setup.sh
+++ ifc/sdg/qifc/joana.ifc.sdg.qifc.qif_interpreter/setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 # installs cryptominisat + approxmc
-# the following packages need to be installed:
+# the follwoing packages need to be installed:
 # build-essential cmake zlib1g-dev libboost-program-options-dev libm4ri-dev
 
 git clone https://github.com/msoos/cryptominisat
-cd cryptominisat && mkdir build
-cd cryptominisat/build && cmake .. && make && make install
+cd cryptominisat && mkdir build && cd build && cmake .. && make && make install
 
-git clone https://github.com/meelgroup/approxmc/
-cd approxmc && mkdir build
-cd approxmc/build && cmake .. && make && make install
+git clone https://github.com/meelgroup/approxmc
+cd approxmc && mkdir build && cd build && cmake .. && make && make install
